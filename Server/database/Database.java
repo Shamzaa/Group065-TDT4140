a8f5e74d7c94832edc9c1457da8d4c28b3e07d73
Peder Gjerstad <pgjerstad@gmail.com>
2017-02-18 18:11:31 +0100

    Added class Database, included \.metadata/ in .gitignore
    Database requires jdbc-archive for mysql.

---
+++ Server/database/Database.java
@@ -0,0 +1,65 @@
+package database;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+
+
+/**
+ * @author pgjerstad
+ *
+ * For å bruke koblingen mot MySQL må alle som skal kjøre ServerMain, egentlig Database, ha mysql-connector-java-5.1.40-bin.jar på classpath.
+ */
+
+public class Database implements AutoCloseable {
+	Connection conn = null;
+	
+	public static void main(String[] args) throws Exception {  // Denne main-klassen eksisterer kun for testformål.
+		System.out.println("test");
+		try (Database db = new Database()) {   // Fordi den implementerer AutoCloseable, vil den automatisk close.
+			db.connect();
+			db.testSporring();
+		}
+	}
+	
+	public void connect() throws Exception {
+		// Class.forName("com.mysql.jdbc.Driver");  Not needed if Eclipse?
+		try {
+			conn = DriverManager.getConnection("jdbc:mysql://mysql.stud.ntnu.no/pedergj_hearme_db?user=pedergj_hearme&password=hearme");
+		}
+		catch (SQLException ex) {
+			System.out.println("SQLEcxeption: " + ex.getMessage());
+		}
+	}
+	
+	public void testSporring() {
+		try (Statement stmt = conn.createStatement()) {
+			String query = "SELECT question, time FROM questions";
+			if (stmt.execute(query)) {
+				try (ResultSet rs = stmt.getResultSet();) {
+					while (rs.next()) {
+						String question = rs.getString(1);
+						String time = rs.getString(2);
+						System.out.println(question + " - " + time);
+					}					
+				}
+			}
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void close() {
+		try {
+			if (conn != null)
+			conn.close();
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+		
+	}
+}
