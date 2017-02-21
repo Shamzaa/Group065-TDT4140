5376a3ed3e0a5f5fc814e5026e34054b23bf744b
Peder Gjerstad <pgjerstad@gmail.com>
2017-02-21 09:15:42 +0100

    Created method to post new question to database

--- Server/database/Database.java
+++ Server/database/Database.java
@@ -16,6 +16,7 @@ import java.sql.Statement;
  * 
  */
 
+
 public class Database implements AutoCloseable {
 	Connection conn = null;
 	private static String KEY_URL = DatabaseKey.KEY_URL;
@@ -24,7 +25,7 @@ public class Database implements AutoCloseable {
 		System.out.println("test");
 		try (Database db = new Database()) {   // Fordi den implementerer AutoCloseable, vil den automatisk close.
 			db.connect();
-			db.testSporring();
+			db.postNewQuestion("Hei, hei hei. Dette er et spørsmål", 1);
 		}
 	}
 	
@@ -55,6 +56,26 @@ public class Database implements AutoCloseable {
 			e.printStackTrace();
 		}
 	}
+	
+/**
+ * postNewQuestion() takes in the question as a string and the lecture_id as an int, and posts
+ * the question to the database. Returns true if successful, false otherwise.
+ */
+	
+	protected boolean postNewQuestion(String question, int lecture_id) {
+		try (Statement stmt = conn.createStatement()) {
+			String query = "insert into questions(question, lecture_id, time) values ('" + question + "'," + lecture_id + ", NOW());";
+			System.out.println(query);
+			if (stmt.execute(query)) {
+				return true;
+			}					
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+		return false;
+	}
+
 
 	@Override
 	public void close() {
