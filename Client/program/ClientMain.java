f50242a9a05568f54a4e521e8214ebb16c858e8d
Anders Rantala Hunderi <andersrahu@gmail.com>
2017-03-22 17:23:42 +0100

    UI Changes
    - Roleselector now centered and better suited for the css layout

--- Client/program/ClientMain.java
+++ Client/program/ClientMain.java
@@ -75,9 +75,6 @@ public class ClientMain extends Application{
 		stage.setScene(scene);
 		stage.show();
 		loadUI("ui/RoleSelector.fxml");
-		
-		
-		
 	}
 	
 	public ServerManager getServerManager(){
