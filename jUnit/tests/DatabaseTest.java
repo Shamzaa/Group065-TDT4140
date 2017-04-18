d1491dfead48465d0751cd7eec97cbb428326583
Anders Rantala Hunderi <andersrahu@gmail.com>
2017-04-18 23:17:35 +0200

    Added a suite to run all test. Can now add new testCase classes as needed and run them all via MainTestSuite

--- jUnit/tests/DatabaseTest.java
+++ jUnit/tests/DatabaseTest.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.*;
 import java.util.ArrayList;
 import java.util.Map;
 
+import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -17,68 +19,76 @@ import database.Database;
  * the NTNU network.
  */
 public class DatabaseTest {
+	//--------------------------------------------------------------------------------------
+	//Setup and Teardown
+	static Database db;
+	/**
+	 * Initializes values for ALL tests (runs once)
+	 */
 	@BeforeClass
-	public void setUpClass(){
-		//Initializes values for ALL tests (runs once)
-	}
-	
-	@Before
-	public void setUp(){
-		//Initializes values before each test (thus runs once for each test)
-	}
-	
-	//Database db = new Database();
-	
-	
-	
-	
-	
-	/*public void setUp() {
+	public static void setUpClass(){
 		db = new Database();
 		db.connect();
-		assertTrue(db instanceof Database);
-	}*/
-	@Test
-	public void testSetup() throws Exception {
 		
+		assertTrue(db instanceof Database);		
+	}
+	/**
+	 * Initializes values before each test (thus runs once for each test)
+	 */
+	@Before
+	public void setUp(){
+		//Empty
 	}
 	
+	/**
+	 * This method runs after each test (thus runs once for EACH test)
+	 */
+	@After
+	public void tearDown(){
+		// Empty
+	}
 	
-	/*
+	/**
+	 * This method runs when all tests have been run (Thus runs only once)
+	 */
+	@AfterClass
+	public static void tearDownClass() {
+		db.connect();
+		db.executeStatement("delete from questions where question = '#test'");
+		db.close();
+	}	
 	
-	public void testConnection() {
-		assertTrue(db.connect()); 
+	//--------------------------------------------------------------------------------------
+	//TEST CASES 
+	@Test
+	public void testConnection() throws Exception {
+		assertTrue(db.connect()); 		
 	}
 	
 	@Test
-	public void testName() throws Exception {
-		
-	}
-	public void testCreateNewSubject() {
+	public void testCreateNewSubject() throws Exception {
 		assertFalse(db.executeStatement("delete from subjects where code = 'TEST001'"));
 		assertTrue(db.createNewSubject("TEST001", "TestName"));
 		assertFalse(db.createNewSubject("TEST001", "TestName")); //Should rejects duplicte subjects
-		assertFalse(db.createNewSubject("TEST0010000000", "TestName")); // Should reject too long codes
+		assertFalse(db.createNewSubject("TEST0010000000", "TestName")); // Should reject too long codes		
 	}
 	
-	public void testCreateNewLecture() {
+	@Test
+	public void testCreateNewLecture() throws Exception {
+		
+		/* Not sure what's being tested here, TODO later
 		assertFalse(db.executeStatement("delete from lectures where subject_code = 'TEST001'"));
 		db.createNewLecture("TEST001");
-		assertTrue(db.createNewLecture("NonExistingSubject") == 0); //Should reject nonexisting subjects
+		assertTrue(db.createNewLecture("NonExistingSubject") == 0); //Should reject nonexisting subjects*/		
 	}
 	
-	public void testPostNewQuestion() {
+	@Test
+	public void testPostNewQuestion() throws Exception {
 		db.postNewQuestion("#test", 1);
 		ArrayList<Map<String, String>> question = db.getLastestQuestions(1, 1);
 		assertEquals(question.size(), 1); // The retrieval should have a result present
 		assertEquals("#test", question.get(0).get("question"));
 		assertTrue(question.get(0).get("time") instanceof String);
-		assertEquals("0", question.get(0).get("rating"));				
+		assertEquals("0", question.get(0).get("rating"));						
 	}
-	
-	public void tearDown() {
-		db.connect();
-		db.executeStatement("delete from questions where question = '#test'");
-		db.close();
-	}*/
 }
