1d304c68023afb4ee4a465267bc03c3b51de4e8b
Erling Hærnes Ihlen <erlingih@stud.ntnu.no>
2017-04-25 15:49:10 +0200

    untrack databasekey

--- Server/program/database/DatabaseKey.java
+++
@@ -1,7 +0,0 @@
-package program.database;
-
-public class DatabaseKey {
-	
-	public static String KEY_URL = "jdbc:mysql://mysql.stud.ntnu.no/pedergj_hearme_db?user=pedergj_hearme&password=LZMZ05wamRzMI3If";
-
-}
